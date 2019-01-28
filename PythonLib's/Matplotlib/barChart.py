0da01824ae6f5f7bb4a646c8fb17192160e11f89
prasadesk <prasad.progdesk@gmail.com>
2019-01-28 11:33:55 +0530

    Prasad.txt Added

---
+++ PythonLib's/Matplotlib/barChart.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [2, 7, 4, 9, 1, 6]
+y = [1, 3, 5, 7, 9]
+plt.plot(y, label="bar chart", color="red")
+plt.plot(x, label="bar chart", color="blue")
+
+plt.xlabel = "pras"
+
+plt.legend()
+plt.show()
